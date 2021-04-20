b7bab1c39fdca4c99229cbbe9e8077519b9aafa4
Anh7999 <55305554+Anh7999@users.noreply.github.com>
2021-04-20 21:32:30 +0700

    md

--- busproject2/Areas/Admin/Controllers/MapController.cs
+++ busproject2/Areas/Admin/Controllers/MapController.cs
@@ -1,4 +1,6 @@
-﻿using busproject2.Models;
+﻿
+using busproject2.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +9,20 @@ using System.Web.Mvc;
 
 namespace busproject2.Areas.Admin.Controllers
 {
-    public class MapController : BaseController
+    public class MapController : Controller
     {
         // GET: Admin/Map
         private Model1 db = new Model1();
 
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-           ViewBag.ListTuyen = TuyenXe.getAllTuyen();
+            ViewBag.ListTuyen = TuyenXe.getAllTuyen();
+            if (id != null)
+            {
+                ViewBag.XeByMaTuyen = Xe.GetAllBusByMaTuyen((int)id);
+            }
+            ViewBag.ListXe = JsonConvert.SerializeObject(Xe.GetAllBus());
             return View();
         }
-
-      
-
     }
 }
